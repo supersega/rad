2482e81696dc73e41091bb86e50f2f9e9d0939ca
Serhii Malyshev <malyshev13091995@gmail.com>
2020-04-06 20:15:11 +0300

    [rad] implement from trait for Dual number

--- src/dual.rs
+++ src/dual.rs
@@ -1,6 +1,6 @@
 
 /// Dual number representation.
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Dual {
     /// Value of Dual number.
     val: f64,
@@ -22,9 +22,30 @@ impl Dual {
     }
 }
 
+impl From<f64> for Dual {
+    fn from(val: f64) -> Self {
+        Self { 
+            val, 
+            der: 0.0,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+use super::*;
 #[test]
 fn test_create_new_dual() {
     let x = Dual::new(1.0);
     assert_eq!(x.val, 1.0);
     assert_eq!(x.der, 0.0);
+}
+
+#[test]
+fn test_from_into_dual() {
+    let val = 2.0;
+    let dual1: Dual = val.into();
+    let dual2 = Dual::from(val);
+    assert_eq!(dual1, dual2);
+}
 }
