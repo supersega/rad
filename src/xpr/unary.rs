1811c15f54c7c185e78e5ca621701c55c68e5cba
Serhii Malyshev <malyshev13091995@gmail.com>
2020-04-15 23:50:20 +0300

    [rad] Implement sin operation

--- src/xpr/unary.rs
+++ src/xpr/unary.rs
@@ -51,6 +51,52 @@ macro_rules! impl_un_op(
 
 impl_un_op!(Neg, neg, NegXpr);
 
+/// Sinus expression
+#[derive(Copy, Clone, Debug)]
+pub struct SinXpr<Arg> where 
+    Arg: Xpr + Copy + Clone {
+    arg: Arg,
+}
+
+impl<Arg> Xpr for SinXpr<Arg> where 
+    Arg: Xpr + Copy + Clone {
+    fn value(&self) -> f64 { self.arg.value().sin() }
+}
+
+impl<E> Assign for SinXpr<E> where 
+    E: Xpr + Copy + Clone + Assign {
+    fn assign(&self, other: &mut Dual) {
+        self.arg.assign(other);
+        other.val = other.val.sin();
+        other.der *= other.val.cos();
+    }
+
+    fn assign_add(&self, target: &mut Dual) {
+        let mut aux: Dual = 0.0.into();
+        self.assign(&mut aux);
+        aux.assign_add(target);
+    }
+
+    fn assign_sub(&self, target: &mut Dual) {
+        let mut aux: Dual = 0.0.into();
+        self.assign(&mut aux);
+        aux.assign_sub(target);
+    }
+}
+
+macro_rules! define_and_impl_un_op (
+    ($Op: ident, $op: ident, $Res: ident) => {
+        /// $Op operation
+        pub trait $Op {
+            type Output;
+            fn $op(self) -> Self::Output;
+        }
+        impl_un_op!($Op, $op, $Res);
+    };
+);
+
+define_and_impl_un_op!(Sin, sin, SinXpr);
+
 #[cfg(test)]
 mod tests {
 use super::*;
@@ -62,5 +108,17 @@ fn test_value_from_neg_expressions() {
     let d = Dual::from(-(b - a));
 
     assert_eq!(c.val, d.val);
+}
+
+#[test]
+fn test_sin_xpr(){
+    let a = Dual::from(1.0); 
+    let sina1 = a.sin();
+    let sina2 = Dual::sin(a);
+    assert_eq!(sina1.value(), sina2.value());
+    let b = Dual::from(2.0);
+    let sinab1 = (a + b).sin();
+    let sinab2 = XprWrapper::sin(a + b);
+    assert_eq!(sinab1.value(), sinab2.value());
 }
 }
